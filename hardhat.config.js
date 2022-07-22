d116c7893030ecccbf8c81d2d5ada045d94993ea
b-machine-learning <eb9uxkwy@addymail.com>
2022-07-22 11:37:53 -0500

    Token Deployed to Local

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,8 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {
+        url: "http://127.0.0.1:8545"}
+  },
 };
